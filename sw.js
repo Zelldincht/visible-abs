735e1fcad6bc68d10c911ca0145e990b7d75d45b
zelldincht <zelldincht@users.noreply.github.com>
2026-07-12 01:06:52 +0100

    Force refresh of themed PWA assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'goalrpg-shell-v3';
-const SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/goalrpg.png'];
+const CACHE = 'goalrpg-shell-v4';
+const SHELL = ['./', './index.html', './styles.css?v=4', './app.js?v=4', './manifest.webmanifest?v=4', './icons/goalrpg.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
